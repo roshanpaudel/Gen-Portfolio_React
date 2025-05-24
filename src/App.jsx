759623cbfa12cdd4f11cc2e1f5eb13461b9f9c4e
Roshan Paudel <paudelroshan93@gmail.com>
2025-05-24 20:06:30 +1000

    html component properties changed to react format

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,24 @@ function App() {
     <>
       <input type="checkbox" id="darkmode" />
 
-      <div class="wrapper">
-        <label for="darkmode">
-          <i class="fa-solid fa-circle-half-stroke"></i>
+      <div className="wrapper">
+        <label htmlFor="darkmode">
+          <i className="fa-solid fa-circle-half-stroke"></i>
         </label>
         {/* <!-- navbar --> */}
-        <div class="navbar">
-          <div class="navigation flex-center container">
-            <div class="nav-title flex-center">
-              <p class="name">Roshan</p>
-              <p class="slash">|</p>
-              <p class="job-title">Full Stack Developer</p>
+        <div className="navbar">
+          <div className="navigation flex-center container">
+            <div className="nav-title flex-center">
+              <p className="name">Roshan</p>
+              <p className="slash">|</p>
+              <p className="job-title">Full Stack Developer</p>
             </div>
 
-            <label for="hamburgerMenu">
-              <i class="fa-solid fa-bars"></i>
+            <label htmlFor="hamburgerMenu">
+              <i className="fa-solid fa-bars"></i>
             </label>
             <input type="checkbox" name="" id="hamburgerMenu" />
-            <div class="nav-link">
+            <div className="nav-link">
               <ul>
                 <li>
                   <a href="#home">Home</a>
@@ -46,118 +46,118 @@ function App() {
             </div>
           </div>
         </div>
-        <div class="page-container">
+        <div className="page-container">
           {/* <!-- hero  --> */}
           <section id="home">
-            <div class="hero grid container">
-              <div class="banner flex">
-                <p class="intro">
+            <div className="hero grid container">
+              <div className="banner flex">
+                <p className="intro">
                   Hi I'm
-                  <span style="font-weight: bold">Roshan Paudel</span>
+                  <span style={{ fontWeight: "bold" }}>Roshan Paudel</span>
                 </p>
 
-                <h3 style="letter-spacing: 3.6px">Full Stack Developer</h3>
+                <h3 style={{ letterSpacing: "3.6px" }}>Full Stack Developer</h3>
                 <p>I love coding and tech related stuff</p>
                 <a href="">
                   <button>
-                    Download CV <i class="fa-solid fa-download"></i>
+                    Download CV <i className="fa-solid fa-download"></i>
                   </button>
                 </a>
               </div>
-              <div class="image flex">
+              <div className="image flex">
                 <img src="assets/Roshan.png" alt="Roshan" />
               </div>
             </div>
           </section>
 
           {/* <!-- badge  --> */}
-          <div class="badge container flex-center">
-            <div class="skillset">
+          <div className="badge container flex-center">
+            <div className="skillset">
               <p>✪</p>
-              <div class="skill-name">
+              <div className="skill-name">
                 <p>IT</p>
-                <p class="skill-property">Graduation</p>
+                <p className="skill-property">Graduation</p>
               </div>
             </div>
-            <div class="divider"></div>
-            <div class="skillset">
+            <div className="divider"></div>
+            <div className="skillset">
               <p>✪</p>
-              <div class="skill-name">
+              <div className="skill-name">
                 <p>5+ Projects</p>
-                <p class="skill-property">Completed</p>
+                <p className="skill-property">Completed</p>
               </div>
             </div>
-            <div class="divider"></div>
-            <div class="skillset">
+            <div className="divider"></div>
+            <div className="skillset">
               <p>✪</p>
-              <div class="skill-name">
+              <div className="skill-name">
                 <p>1+ Year</p>
-                <p class="skill-property">Experience</p>
+                <p className="skill-property">Experience</p>
               </div>
             </div>
           </div>
 
           {/* <!-- skills  --> */}
           <section id="skills">
-            <div class="title-header flex skills-section">
+            <div className="title-header flex skills-section">
               <h2>Skills</h2>
             </div>
-            <div class="skill-icons flex-center container">
-              <div class="icon-text">
-                <i class="fa-brands fa-html5"></i>
+            <div className="skill-icons flex-center container">
+              <div className="icon-text">
+                <i className="fa-brands fa-html5"></i>
                 <p>HTML</p>
               </div>
-              <div class="icon-text flex-center">
-                <i class="fa-brands fa-css"></i>
+              <div className="icon-text flex-center">
+                <i className="fa-brands fa-css"></i>
                 <p>CSS</p>
               </div>
-              <div class="icon-text flex-center">
-                <i class="fa-brands fa-js"></i>
+              <div className="icon-text flex-center">
+                <i className="fa-brands fa-js"></i>
                 <p>Javascript</p>
               </div>
-              <div class="icon-text flex-center">
-                <i class="fa-brands fa-github"></i>
+              <div className="icon-text flex-center">
+                <i className="fa-brands fa-github"></i>
                 <p>Github</p>
               </div>
-              <div class="icon-text flex-center">
-                <i class="fa-brands fa-figma"></i>
+              <div className="icon-text flex-center">
+                <i className="fa-brands fa-figma"></i>
                 <p>fa-figma</p>
               </div>
             </div>
           </section>
 
           {/* <!-- my-projects  --> */}
-          <div class="projects container" id="projects">
-            <div class="title-header flex">
+          <div className="projects container" id="projects">
+            <div className="title-header flex">
               <h2>My Projects</h2>
             </div>
-            <div class="grid my-projects">
-              <div class="projects-card">
+            <div className="grid my-projects">
+              <div className="projects-card">
                 <img src="assets/GlideShift.JPG" alt="Project 1" />
-                <div class="tech-link">
+                <div className="tech-link">
                   <a href="#">
-                    <i class="fa-brands fa-github"></i>
+                    <i className="fa-brands fa-github"></i>
                   </a>
                   <a href="#">
-                    <i class="fa-brands fa-chrome"></i>
+                    <i className="fa-brands fa-chrome"></i>
                   </a>
                 </div>
-                <div class="project-info">
+                <div className="project-info">
                   <h3>GlideShift HTML Template</h3>
                   <p>Techstack: HTML5, CSS</p>
                 </div>
               </div>
-              <div class="projects-card">
+              <div className="projects-card">
                 <img src="assets/HikeCo.JPG" alt="Project 1" />
-                <div class="tech-link">
+                <div className="tech-link">
                   <a href="#">
-                    <i class="fa-brands fa-github"></i>
+                    <i className="fa-brands fa-github"></i>
                   </a>
                   <a href="#">
-                    <i class="fa-brands fa-chrome"></i>
+                    <i className="fa-brands fa-chrome"></i>
                   </a>
                 </div>
-                <div class="project-info">
+                <div className="project-info">
                   <h3>GlideShift HTML Template</h3>
                   <p>Techstack: HTML5, CSS</p>
                 </div>
@@ -167,16 +167,16 @@ function App() {
         </div>
 
         {/* <!-- about-me  --> */}
-        <div class="about-container" id="about">
-          <div class="title-header about-title flex">
+        <div className="about-container" id="about">
+          <div className="title-header about-title flex">
             <h2>About me</h2>
           </div>
 
-          <div class="about-me flex container">
-            <div class="my-photo">
+          <div className="about-me flex container">
+            <div className="my-photo">
               <img src="assets/Roshan.png" alt="My photo" />
             </div>
-            <div class="my-details flex">
+            <div className="my-details flex">
               <h1>Roshan Paudel</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem
@@ -186,7 +186,7 @@ function App() {
               </p>
               <h3>Toowoomba, Australia</h3>
               <h3>Interest</h3>
-              <div class="interest flex">
+              <div className="interest flex">
                 <span>Coding</span>
                 <span>Football</span>
                 <span>Scifi movies</span>
@@ -196,19 +196,19 @@ function App() {
         </div>
 
         {/* <!-- contact  --> */}
-        <div class="contact flex-center" id="contact">
-          <div class="title-header contact-title">
+        <div className="contact flex-center" id="contact">
+          <div className="title-header contact-title">
             <h2>Contact</h2>
           </div>
-          <div class="contact-links">
-            <i class="fa-brands fa-github"></i>
-            <i class="fa-brands fa-linkedin"></i>
-            <i class="fa-solid fa-mobile"></i>
+          <div className="contact-links">
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-solid fa-mobile"></i>
           </div>
           <p>OR</p>
 
           <form>
-            <div class="email">
+            <div className="email">
               <input
                 type="email"
                 id="email"
@@ -217,16 +217,16 @@ function App() {
                 required
               />
               <a href="#">
-                <i class="fa-solid fa-paper-plane"></i>
+                <i className="fa-solid fa-paper-plane"></i>
               </a>
             </div>
           </form>
         </div>
 
         {/* <!-- footer  --> */}
-        <div class="footer flex-center">
-          <div class="footer-links-socials flex">
-            <div class="footer-links">
+        <div className="footer flex-center">
+          <div className="footer-links-socials flex">
+            <div className="footer-links">
               <h3>Links</h3>
               <ul>
                 <li>
@@ -243,7 +243,7 @@ function App() {
                 </li>
               </ul>
             </div>
-            <div class="footer-social">
+            <div className="footer-social">
               <h3>Social Links</h3>
 
               <ul>
@@ -262,15 +262,15 @@ function App() {
               </ul>
             </div>
           </div>
-          <div class="copyright">
+          <div className="copyright">
             <p>&copy; All rights reserved. Made by Roshan 🤘🏽</p>
           </div>
         </div>
 
         {/* <!-- go UP --> */}
-        <div class="go-up">
-          <a class="flex-center" href="#home">
-            <i class="fa-solid fa-chevron-up"></i>
+        <div className="go-up">
+          <a className="flex-center" href="#home">
+            <i className="fa-solid fa-chevron-up"></i>
           </a>
         </div>
       </div>
